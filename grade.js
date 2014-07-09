a4f3a26961e87a197966f3a5fda917b01b26bf86
indotek68 <angga.w.putra@gmail.com>
2014-07-08 19:08:28 -0700

    Part 1 done

---
+++ grade.js
@@ -0,0 +1,17 @@
+var grade = 89 
+
+if(grade > 0 && grade < 59){
+	console.log("F");
+}
+else if(grade > 59 && grade <= 69){
+	console.log("D");
+}
+else if(grade > 69 && grade <= 79){
+	console.log("C");
+}
+else if(grade > 79 && grade <= 89){
+	console.log("B");
+}
+else{
+	console.log("A");
+}
---
+++ tempConvert.js
@@ -0,0 +1,4 @@
+var celcius = 32
+var fahrenheit = celcius*1.8 + 32
+
+console.log(celcius + "C & " + fahrenheit + "F")
