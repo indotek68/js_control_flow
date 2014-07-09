86b09c4fda02d5271e44c6ba9cf32c1e3a5938a7
indotek68 <angga.w.putra@gmail.com>
2014-07-09 08:39:07 -0700

    switch

--- grade.js
+++ grade.js
@@ -1,17 +1,39 @@
-var grade = 89 
+var grade = 50 
 
-if(grade > 0 && grade < 59){
-	console.log("F");
-}
-else if(grade > 59 && grade <= 69){
-	console.log("D");
-}
-else if(grade > 69 && grade <= 79){
-	console.log("C");
-}
-else if(grade > 79 && grade <= 89){
-	console.log("B");
-}
-else{
-	console.log("A");
+// if(grade > 0 && grade < 59){
+// 	console.log("F");
+// }
+// else if(grade > 59 && grade <= 69){
+// 	console.log("D");
+// }
+// else if(grade > 69 && grade <= 79){
+// 	console.log("C");
+// }
+// else if(grade > 79 && grade <= 89){
+// 	console.log("B");
+// }
+// else{
+// 	console.log("A");
+// }
+
+switch (grade) 
+{
+  case grade > 0 && grade < 59:
+    dconsole.log("F");
+    break;
+  case grade > 59 && grade <= 69:
+    console.log("D");
+    break;
+  case grade > 69 && grade <= 79:
+    console.log("C");
+    break;
+  case grade > 79 && grade <= 89:
+    console.log("B");
+    break;
+  case grade > 89 && grade <= 100:
+    console.log("A");
+    break;
+  default:
+    console.log("Not a standard grade");
+    break;
 }
--- reverse.js
+++ reverse.js
@@ -2,7 +2,7 @@ var inputString = "building";
 var reverse = ""
 
  for(var i = inputString.length-1; i >= 0; i--){
-  	reverse += inputString[i];
+  	reverse = reverse + inputString[i];
   }
 console.log(reverse)
 
