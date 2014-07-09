248f7e7b0d7b719c6e2a82f7f17ba71f78e7f3d8
indotek68 <angga.w.putra@gmail.com>
2014-07-08 18:39:27 -0700

    Google Done

---
+++ filterLongWords.js
@@ -0,0 +1,8 @@
+var maxLength = 4
+var wordsArray = ["orange", "apple", "kiwi", "strawberry", "pie", "potato", "eggs"];
+
+for(i = 0; i < wordsArray.length; i++){
+	if (wordsArray[i].length <= maxLength){
+		console.log(wordsArray[i])
+	}
+}
--- google_shopping.js
+++ google_shopping.js
@@ -1,5 +1,66 @@
 var data = require("./products.json")
+var count = 0;
 
-// Write your solutions below
 
-console.log(data["items"]);
+// Count items
+for(var i = 0; i < data.items.length; i++){
+	if (data.items[i].kind === "shopping#product"){
+		count++;
+	}
+}
+console.log(count)
+
+console.log("-----------------")
+//list items that are in back order
+for(var i = 0; i < data.items.length; i++){
+	if (data.items[i].product.inventories[0].availability === "backorder"){
+		console.log(data.items[i].product.title)
+	}
+}
+console.log("-----------------")
+for(var i = 0; i < data.items.length; i++){
+	if (data.items[i].product.images.length > 1){
+		console.log(data.items[i].product.title)
+	}
+}
+console.log("-----------------")
+
+for(var i = 0; i < data.items.length; i++){
+	if (data.items[i].product.brand === "Canon"){
+		console.log(data.items[i].product.title)
+	}
+}
+console.log("-----------------")
+
+for(var i = 0; i < data.items.length; i++){
+	if (data.items[i].product.brand === "Canon" && data.items[i].product.author.name === "eBay" ){
+		console.log(data.items[i].product.title);
+	}
+}
+console.log("-----------------")
+
+for(var i = 0; i < data.items.length; i++){
+		console.log(data.items[i].product.brand);
+		console.log(data.items[i].product.inventories[0].price)
+		for(var j = 0; j < data.items[i].product.images.length; j++){
+			console.log(data.items[i].product.images[j]);
+		}	
+}
+console.log("-----------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
